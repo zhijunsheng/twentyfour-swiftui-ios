c726f426edaebbe0e393606a0d3a311b6bc02402
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-21 07:42:49 -0500

    created SwiftUI app for 24 game

---
+++ TwentyFour/TwentyFour/TwentyFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwentyFourApp.swift
+//  TwentyFour
+//
+//  Created by Golden Thumb on 2020-11-21.
+//
+
+import SwiftUI
+
+@main
+struct TwentyFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

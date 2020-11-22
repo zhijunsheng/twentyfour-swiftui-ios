486f2f7976978e85a30b8d8e8d123de4c3496f73
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-22 06:43:39 -0500

    created TextTwentyFourGameView

---
+++ TwentyFour/TwentyFour/TextTwentyFourGameView.swift
@@ -0,0 +1,20 @@
+//
+//  TextTwentyFourGameView.swift
+//  TwentyFour
+//
+//  Created by Golden Thumb on 2020-11-22.
+//
+
+import SwiftUI
+
+struct TextTwentyFourGameView: View {
+    var body: some View {
+        Text("8")
+    }
+}
+
+struct TextTwentyFourGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextTwentyFourGameView()
+    }
+}

031670beda894a6b8146c9a6f3c5654830d058dc
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-23 09:32:15 -0500

    made the 24 game include J Q K

--- TwentyFour/TwentyFour/ImageTwentyFourGame.swift
+++ TwentyFour/TwentyFour/ImageTwentyFourGame.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 class ImageTwentyFourGame {
-    private var game = TwentyFourGame<Image>(cardContentFactory: { (num) -> Image in
-        let numStr: String = num == 1 ? "A" : "\(num)"
+    private var game = TwentyFourGame<Image>() { (num) -> Image in
+        var numStr: String
+        
+        switch num {
+        case 1:
+            numStr = "A"
+        case 11:
+            numStr = "J"
+        case 12:
+            numStr = "Q"
+        case 13:
+            numStr = "K"
+        default:
+            numStr = "\(num)"
+        }
+        
         let suit: String = ["S", "H", "D", "C"][Int(arc4random()) % 4]
         return Image(numStr + suit)
-    })
+    }
     
     var card0: TwentyFourGame<Image>.Card { game.card0 }
     var card1: TwentyFourGame<Image>.Card { game.card1 }
--- TwentyFour/TwentyFour/TwentyFourGame.swift
+++ TwentyFour/TwentyFour/TwentyFourGame.swift
@@ -8,22 +8,24 @@
 import Foundation
 
 struct TwentyFourGame<CardContent> {
+    private let maxNum: Int = 13
+    
     private(set) var card0: Card
     private(set) var card1: Card
     private(set) var card2: Card
     private(set) var card3: Card
     
     init(cardContentFactory: (Int) -> CardContent) {
-        let num0: Int = Int(arc4random() % 10 + 1)
+        let num0: Int = Int(arc4random()) % maxNum + 1
         card0 = Card(number: num0, content: cardContentFactory(num0))
         
-        let num1: Int = Int(arc4random() % 10 + 1)
+        let num1: Int = Int(arc4random()) % maxNum + 1
         card1 = Card(number: num1, content: cardContentFactory(num1))
         
-        let num2: Int = Int(arc4random() % 10 + 1)
+        let num2: Int = Int(arc4random()) % maxNum + 1
         card2 = Card(number: num2, content: cardContentFactory(num2))
         
-        let num3: Int = Int(arc4random() % 10 + 1)
+        let num3: Int = Int(arc4random()) % maxNum + 1
         card3 = Card(number: num3, content: cardContentFactory(num3))
     }
     
